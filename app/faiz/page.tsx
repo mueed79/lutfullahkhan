10e281f5e365fc7badfd1b5e9dfb0952640bcdf9
mueed79 <mueed.rehman@arbisoft.com>
2026-05-10 20:21:29 +0500

    commit at 8:21

--- app/faiz/page.tsx
+++ app/faiz/page.tsx
@@ -66,7 +66,7 @@ export default function FaizPage() {
           </motion.div>
         </div>
 
-        <div className="relative z-10 max-w-[900px] text-center mb-24">
+        <div className="relative z-20 max-w-[900px] text-center mb-24">
           <motion.div
             initial={{ opacity: 0, y: 30 }}
             whileInView={{ opacity: 1, y: 0 }}
